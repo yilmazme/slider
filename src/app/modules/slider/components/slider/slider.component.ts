4a1d7d20d6dca525873ff64330ae6de4c525ccbc
mehmet yilmaz <10myilmaz@gmail.com>
2022-01-08 10:31:02 +0300

    only service changed to slider-service

--- src/app/modules/slider/components/slider/slider.component.ts
+++ src/app/modules/slider/components/slider/slider.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { SliderService } from '../../services/slider.service';
+import { SliderService } from '../../slider-services/slider.service';
 import { Product } from 'src/models/Product';
 
 @Component({
